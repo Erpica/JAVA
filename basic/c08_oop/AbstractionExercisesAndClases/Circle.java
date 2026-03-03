efa0c607cadf1e0a12dc083b894215f06c3456e4
erpica <erpica@gmail.com>
2026-03-03 17:14:15 +0100

    Tras muchos problemas con packcages al mover una carpeta entera con muchas clases, casi todo funciona.

--- basic/c08_oop/AbstractionExercisesAndClases/Circle.java
+++ basic/c08_oop/AbstractionExercisesAndClases/Circle.java
@@ -3,6 +3,7 @@ package c08_oop.AbstractionExercisesAndClases;
 // 1. Crea una clase abstracta Shape con el método calculateArea().
 //  Luego implementa dos subclases: Circle y Rectangle, y haz que cada una calcule su propia área.
 
+
 public class Circle extends Shape{
     double radius;
 
